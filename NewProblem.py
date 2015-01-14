9de73b488894002741e06e62d409f91627e878a1
Nick Crawford <n.crawfish@gmail.com>
2015-01-13 19:36:50 -0800

    Restructure repo

--- NewProblem.py
+++ NewProblem.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-doc = """
-'''
-http://projecteuler.net/problem=%s
+doc = """'''
+http://projecteuler.net/problem={}
 Solution: N/A
-Runtime: N/A (with PyPy)
+Runtime: N/A
+With PyPy: N/A
 '''
 import time
 
@@ -16,14 +16,15 @@ def main():
 
 if __name__ == "__main__":
     t1 = time.time()
-    main()
+    sol = main()
     t2 = time.time()
+    print 'Solution: %s' % sol
     print 'Runtime: %0.3f sec' % float((t2 - t1))
 """
 
 def create_file(n):
     f = open('Problem %s.py' % n,'w')
-    f.write(doc)
+    f.write(doc.format(n))
     f.close()
 
 def main():
