3381a81dcef6a3266a06682024708f2dc941f18b
Nick Crawford <n.crawfish@gmail.com>
2016-03-27 19:26:29 -0700

    Better 20, 22

--- Solved/Problem 020.py
+++ Solved/Problem 020.py
@@ -1,22 +1,34 @@
-def factorial(num):
-    total = 1
-    for i in range(num):
-        total *= (i+1)
-    return total
+'''
+http://projecteuler.net/problem=020
+Solution: 648
+Runtime: 0.000 sec
+With PyPy: N/A
+'''
+import time
 
-def break_to_digits(num):
-    nums = []
-    for elem in str(num):
-        nums.append(elem)
-    for i in nums:
-        nums.append(int(nums[0]))
-        nums.remove(nums[0])
-    return nums
+# using std lib
+# from math import factorial
 
-def sum_up(nums):
+# using memoized
+def factorial(n, saved={0: 1, 1: 1}):
+    return saved.setdefault(n, saved.get(n) or n * factorial(n-1))
+
+# using strings
+def main():
+    return sum(int(x) for x in str(factorial(100)))
+
+# only integers (faster)
+def main():
+    x = factorial(100)
     total = 0
-    for elem in nums:
-        total += elem
+    while x:
+        total += x % 10
+        x = x / 10
     return total
 
-print sum_up(break_to_digits(factorial(100)))
+if __name__ == "__main__":
+    t1 = time.time()
+    sol = main()
+    t2 = time.time()
+    print 'Solution: %s' % sol
+    print 'Runtime: %0.3f sec' % float((t2 - t1))
--- Solved/Problem 022.py
+++ Solved/Problem 022.py
@@ -1,70 +1,22 @@
+'''
+http://projecteuler.net/problem=022
+Solution: 871198282
+Runtime: 0.013 sec
+With PyPy: N/A
+'''
+import os
 import time
 
-t1 = time.time()
-f = open('names.txt','r')
-l = f.readline()
-f.close()
-_list = l.replace('"','').split(',')
-_list.sort()
+NAMES = os.path.join('euler', 'resources', 'p022_names.txt')
 
-score_sum = 0
-for name in _list:
-    name_score = 0
-    for letter in name:
-        if letter == 'A':
-            name_score += 1
-        elif letter == 'B':
-            name_score += 2
-        elif letter == 'C':
-            name_score += 3
-        elif letter == 'D':
-            name_score += 4
-        elif letter == 'E':
-            name_score += 5
-        elif letter == 'F':
-            name_score += 6
-        elif letter == 'G':
-            name_score += 7
-        elif letter == 'H':
-            name_score += 8
-        elif letter == 'I':
-            name_score += 9
-        elif letter == 'J':
-            name_score += 10
-        elif letter == 'K':
-            name_score += 11
-        elif letter == 'L':
-            name_score += 12
-        elif letter == 'M':
-            name_score += 13
-        elif letter == 'N':
-            name_score += 14
-        elif letter == 'O':
-            name_score += 15
-        elif letter == 'P':
-            name_score += 16
-        elif letter == 'Q':
-            name_score += 17
-        elif letter == 'R':
-            name_score += 18
-        elif letter == 'S':
-            name_score += 19
-        elif letter == 'T':
-            name_score += 20
-        elif letter == 'U':
-            name_score += 21
-        elif letter == 'V':
-            name_score += 22
-        elif letter == 'W':
-            name_score += 23
-        elif letter == 'X':
-            name_score += 24
-        elif letter == 'Y':
-            name_score += 25
-        elif letter == 'Z':
-            name_score += 26
-    score_sum += name_score * (_list.index(name) + 1)
-        
-print score_sum
-t2 = time.time()
-print 'Runtime: %0.3f sec' % float((t2 - t1))
+def main():
+    with open(NAMES, 'rb') as f:
+        names = sorted(f.read().replace('"', '').split(','))
+        return sum((i+1) * sum((ord(x) - 96) for x in name.lower()) for i, name in enumerate(names))
+
+if __name__ == "__main__":
+    t1 = time.time()
+    sol = main()
+    t2 = time.time()
+    print 'Solution: %s' % sol
+    print 'Runtime: %0.3f sec' % float((t2 - t1))
